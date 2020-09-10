921af5c26e5f8e4964287e03472fbcd7a1df0a80
ronnahom96 <ronnahom96@gmail.com>
2020-09-10 13:13:11 +0300

    final v1

---
+++ src/commands.ts
@@ -0,0 +1,29 @@
+// Query Status
+export const QUERY_BEDROOM_TV_STATUS: string = "Query Bedroom TV status on/off";
+export const QUERY_MICROWAVE_STATUS: string = "Query Microwave status on/off";
+export const QUERY_COMPUTER_STATUS: string = "Query Computer status on/off";
+export const QUERY_LIVING_ROOM_TV_STATUS: string = "Query Living Room TV status on/off";
+export const QUERY_AIR_CONDITIONER_STATUS: string = "Query Air Conditioner status on/off";
+
+// Turn On/Off Status
+export const TURN_ON_OFF_AIR_CONDITIONER: string = "Turn on/off Air Conditioner";
+export const TURN_ON_OFF_BEDROOM_TV: string = "Turn on/off Bedroom TV";
+export const TURN_ON_OFF_MICROWAVE: string = "Turn on/off Microware";
+export const TURN_ON_OFF_COMPUTER: string = "Turn on/off Computer";
+export const TURN_ON_OFF_LIVING_ROOM_TV: string = "Turn on/off Living room TV";
+
+// Query Channel
+export const QUERY_BEDROOM_TV_CHANNEL: string = "Query Bedroom TV channel";
+export const QUERY_LIVING_ROOM_TV_CHANNEL: string = "Query Living Room TV channel";
+
+// Switch Channel
+export const SWITCH_BEDROOM_TV_CHANNEL: string = "Switch channel in Bedroom TV";
+export const SWITCH_LIVING_ROOM_TV_CHANNEL: string = "witch channel in Living room TV";
+
+// Query Channel
+export const QUERY_MICROWAVE_DEGREES: string = "Query Microwave degrees";
+export const QUERY_AIR_CONDITIONER_DEGREES: string = "Query Air Conditioner degrees";
+
+// Switch Channel
+export const SET_MICROWAVE_DEGREE_AND_TIMER: string = "Set degrees and timer to Microwave, no more than 30 degrees";
+export const SET_AIR_CONDITIONER_DEGREE: string = "Set degrees to Air Conditioner, between 10 and 30";
